55ec27ab64270399c0b79335a8eece73b0cf7ead
ushicow <ushicow@nanja.info>
2026-03-03 20:47:03 +0900

    PCB V1.1

---
+++ Gowin/src/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.12.01
+//IP Version: 1.0
+//Part Number: GW1NR-LV9QN88PC6/I5
+//Device: GW1NR-9
+//Device Version: C
+//Created Time: Thu Feb 26 21:18:22 2026
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout), //output clkout
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
--- Gowin/src/top.sv
+++ Gowin/src/top.sv
@@ -4,10 +4,13 @@
 
 module top (
     inout wire [7:0] d,
-    input wire [7:0] ra,
-    input wire [7:0] vid,
+    input wire [7:0] a,
     input wire pras_n,
+    input wire ldps,
     input wire q3,
+    input wire phi0,
+    input wire phi1,
+    input wire en80,
     input wire rw80,
     input wire gr,
     input wire vid80,
@@ -18,7 +21,10 @@ module top (
     input wire c07x,
     input wire ra9,
     input wire ra10,
-    input wire pal,
+    output reg ra_en,
+    output reg vid_en,
+    output wire d_rw,
+    output wire md_en,
     output wire led1,
     output wire led2,
     output wire led3,
@@ -28,46 +34,78 @@ module top (
     input wire mclk
 );
 
-assign led1 = gr;
-assign led2 = vid80;
-assign led3 = an3;
+logic sclk;
+Gowin_rPLL rpll (
+    .clkout(sclk), //output clkout
+    .clkin(clk14m) //input clkin
+);
 
-assign d = (rw80 & ~q3) ? dout : 8'bz;
+assign led1 = gr;
+assign led2 = c07x;
+assign led3 = sync;
+assign led4 = d_rw;
+assign led5 = vid80;
+assign led6 = an3;
+
+assign md_en = en80;
+assign d_rw = ~rw80;
+
+assign d = d_rw ? 8'bz : dout;
 
 logic [7:0] dout;
-logic [9:0] ad;
 logic [7:0] row;
 
 always_ff@(negedge pras_n) begin
-    row <= ra;
+    row <= a;
 end
 
-logic [15:0] addr;      // for debug
+assign ra_en = ~q3;
+assign vid_en = q3;
+logic [7:0] vid;
+always_ff@(negedge ldps) begin
+    vid <= a;
+end
+
+logic [15:0] addr;
+logic [7:0] din;
 always_ff@(negedge q3) begin
-    ad[9:8] <= ra[1:0];
-    ad[7:0] <= row;
-    addr <= {ra, row};  // for debug
+    addr[0] <= row[0];
+    addr[1] <= row[1];
+    addr[2] <= row[2];
+    addr[3] <= row[3];
+    addr[4] <= row[4];
+    addr[5] <= row[5];
+    addr[6] <= a[1];
+    addr[7] <= row[6];
+    addr[8] <= row[7];
+    addr[9] <= a[0];
+    addr[10] <= a[2];
+    addr[11] <= a[3];
+    addr[12] <= a[4];
+    addr[13] <= a[5];
+    addr[14] <= a[6];
+    addr[15] <= a[7];
+    din <= d;
 end
 
 Gowin_RAM16S sram (
     .dout(dout), //output [7:0] dout
     .wre(~rw80), //input wre
-    .ad(ad), //input [9:0] ad
-    .di(d), //input [7:0] di
+    .ad(addr[9:0]), //input [9:0] ad
+    .di(din), //input [7:0] di
     .clk(q3) //input clk
 );
 
-logic [9:0] pix;
-logic [7:0] line;
-logic [9:0] count;
+logic [3:0] cycle;
 always_ff@(posedge clk14m) begin
-    if (sync) begin
-        if (count == 0) begin
-            line <= line + 1;
+    if (phi0) begin
+        if (cycle >= 13) begin
+            cycle = 0;
+        end else begin
+            cycle += 1;
         end
-        count <= count + 1;
     end else begin
-        count <= 0;
+        cycle += 1;
     end
 end
 
