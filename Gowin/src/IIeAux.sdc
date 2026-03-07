276a69667620b14b03b4825e43f13f0412e9b8f7
ushicow <ushicow@nanja.info>
2026-03-07 19:52:48 +0900

    80 Colmun Text

--- Gowin/src/IIeAux.sdc
+++ Gowin/src/IIeAux.sdc
@@ -4,7 +4,5 @@
 //Tool Version: V1.9.12.01 
 //Created Time: 2026-02-14 21:16:13
 create_clock -name clk14m -period 71.429 -waveform {0 35.715} [get_ports {clk14m}]
-create_clock -name mclk -period 37.037 -waveform {0 18.518} [get_ports {mclk}]
 create_clock -name q3 -period 490 -waveform {0 280} [get_ports {q3}]
 create_clock -name pras_n -period 490 -waveform {0 140} [get_ports {pras_n}]
-set_clock_groups -asynchronous -group [get_clocks {mclk}] -group [get_clocks {clk14m q3 pras_n}]
--- Gowin/src/top.sv
+++ Gowin/src/top.sv
@@ -1,70 +1,33 @@
 `default_nettype none
 // Apple IIe Aux Card
-// 2026.01.12 80 Colmun Text
-// 2026.03.06 for V1.2 schematic
+// V1.0.2 2026.03.07 80 Colmun Text
 
 module top (
     inout wire [7:0] d,
-    input wire [7:0] a,
+    input wire [7:0] ar,
     input wire pras_n,
-    input wire serout,
     input wire q3,
-    input wire phi0,
-    input wire phi1,
-    input wire en80,
     input wire rw80,
-    input wire gr,
-    input wire vid80,
-    input wire an3,
-    input wire sync,
-    input wire clk14m,
-    input wire rw,
-    input wire c07x,
-    input wire ra9,
-    input wire ra10,
-    output reg ra_en,
-    output reg vid_en,
-    output wire d_rw,
-    output wire md_en,
-    output wire led1,
-    output wire led2,
-    output wire led3,
-    output wire led4,
-    output wire led5,
-    output wire led6,
-    input wire mclk
+    input wire clk14m
 );
 
-logic sclk;
+logic sclk; // for debug
 Gowin_rPLL rpll (
     .clkout(sclk), //output clkout
     .clkin(clk14m) //input clkin
 );
 
-assign led1 = ra_en;
-assign led2 = vid_en;
-assign led3 = d_rw;
-assign led4 = md_en;
-assign led5 = vid80;
-assign led6 = an3;
-
-assign md_en = en80;
-assign d_rw = ~rw80;
-
-assign d = d_rw ? 8'bz : dout;
-
-logic [7:0] dout;
 logic [7:0] row;
-
-always_ff@(negedge pras_n) begin
-    row <= a;
-end
-
-assign ra_en = 0;
-assign vid_en = 1;
-
 logic [15:0] addr;
 logic [7:0] din;
+logic [7:0] dout;
+
+assign d = rw80 ? dout : 8'bz;
+
+always_ff@(negedge pras_n) begin
+    row <= ar;
+end
+
 always_ff@(negedge q3) begin
     addr[0] <= row[0];
     addr[1] <= row[1];
@@ -72,16 +35,16 @@ always_ff@(negedge q3) begin
     addr[3] <= row[3];
     addr[4] <= row[4];
     addr[5] <= row[5];
-    addr[6] <= a[1];
+    addr[6] <= ar[1];
     addr[7] <= row[6];
     addr[8] <= row[7];
-    addr[9] <= a[0];
-    addr[10] <= a[2];
-    addr[11] <= a[3];
-    addr[12] <= a[4];
-    addr[13] <= a[5];
-    addr[14] <= a[6];
-    addr[15] <= a[7];
+    addr[9] <= ar[0];
+    addr[10] <= ar[2];
+    addr[11] <= ar[3];
+    addr[12] <= ar[4];
+    addr[13] <= ar[5];
+    addr[14] <= ar[6];
+    addr[15] <= ar[7];
     din <= d;
 end
 
